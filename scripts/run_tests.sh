1195004e865549688b6052f78c9b4cfa510a759e
sheecegardezi <sheecegardezi@gmail.com>
2020-01-20 11:40:29 +1100

    BugFix: fix script to run all test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,4 +7,4 @@ export PYTHONPATH=/apps/gdal/3.0.2/lib64/python3.7/site-packages:$PYTHONPAT
 source ~/PyRateVenv/bin/activate
 pip install -U pytest
 cd ~/PyRate
-pytest tests/test_algorithm
+pytest tests/
