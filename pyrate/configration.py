c0c51f520fffdff0fc8e8303937d0762252eb151
sheecegardezi <sheecegardezi@gmail.com>
2020-01-21 17:56:56 +1100

    Feature: add config validation rule

--- pyrate/configration.py
+++ pyrate/configration.py
@@ -14,12 +14,14 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 from configparser import ConfigParser
+from default_parameters import PYRATE_DEFAULT_CONFIGRATION
 
 
 class Configration():
     def __init__(self, config_file_path):
 
         parser = ConfigParser()
+        parser.optionxform = str
         # mimic header to fulfil the requirement for configparser
         with open(config_file_path) as stream:
             parser.read_string("[root]\n" + stream.read())
@@ -27,9 +29,15 @@ class Configration():
         for key, value in parser._sections["root"].items():
             self.__dict__[key] = value
 
+        # check if required values are supplied
+        if not set(PYRATE_DEFAULT_CONFIGRATION).issubset(self.__dict__):
+            raise ValueError("Required configration parameters: " + str(set(PYRATE_DEFAULT_CONFIGRATION).difference(self.__dict__)) + " are missing from input config file.")
+
 if __name__ == "__main__":
     config_file_path = "C:\\Users\\sheec\\Desktop\\Projects\\PyRate\\sample_data\\input_parameters.conf"
     config = Configration(config_file_path)
-
+    print(set(PYRATE_DEFAULT_CONFIGRATION))
     print(config.__dict__)
+
+
 
