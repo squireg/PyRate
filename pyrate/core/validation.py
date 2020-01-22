0659ece55b88318edcc90d714814da3892faee10
sheecegardezi <sheecegardezi@gmail.com>
2020-01-22 17:28:54 +1100

    Feature: add a place holder for future validation functions

---
+++ pyrate/core/validation.py
@@ -0,0 +1,14 @@
+def validate_conv2tif_parameters(params):
+    None
+
+
+def validate_prepifg_parameters(params):
+    None
+
+
+def validate_process_parameters(params):
+    None
+
+
+def validate_merge_parameters(params):
+    None
